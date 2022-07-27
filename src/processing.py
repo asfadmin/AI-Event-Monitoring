9cb221a2e2e6faa799bbfeb1e2dfa6478705b400
Andrew Player <andrewplayer3@gmail.com>
2022-07-27 13:01:00 -0800

    cleaning

--- src/processing.py
+++ src/processing.py
@@ -12,14 +12,14 @@ import numpy as np
 
 
 def tile(
-        arr:        np.ndarray,
-        tile_shape: Tuple[int, int],
-        crop_size:  int   = 0,
-        x_offset:   int   = 0,
-        y_offset:   int   = 0,
-        even_pad:   bool  = False,
-        pad_value:  float = 0.0
-        ) -> Tuple[np.ndarray, int, int]:
+    arr:        np.ndarray,
+    tile_shape: Tuple[int, int],
+    crop_size:  int   = 0,
+    x_offset:   int   = 0,
+    y_offset:   int   = 0,
+    even_pad:   bool  = False,
+    pad_value:  float = 0.0
+) -> Tuple[np.ndarray, int, int]:
 
     """
     Tile a 2-dimensional array into an array of tiles of shape tile_shape.
@@ -59,17 +59,20 @@ def tile(
     rows, cols = len(arr[:, 0]), len(arr[0, :])
 
     if(cols % tile_shape[1] != 0 or rows % tile_shape[0] != 0):
+        
         if x_offset > 0 or y_offset > 0:
             arr_offset = np.zeros((rows + y_offset, cols + x_offset))
             arr_offset[0:rows, 0:cols] = arr
             arr = arr_offset
+        
         arr = pad(
             arr,
             tile_shape,
             even_pad=even_pad,
             extra_padding=(crop_size if cropped else 0),
             value=pad_value
-            )
+        )
+        
         rows, cols = len(arr[:, 0]), len(arr[0, :])
 
     # crop_offset_x => ((dims[1] + crop_size) // 2) - ((dims[1] - crop_size) // 2) = crop_size
@@ -96,8 +99,8 @@ def tile(
             col = j * (crop_size if cropped else tile_shape[1])
             row_start = row + y_offset
             col_start = col + x_offset
-            row_end = row + y_offset + tile_shape[0]
-            col_end = col + x_offset + tile_shape[1]
+            row_end   = row + y_offset + tile_shape[0]
+            col_end   = col + x_offset + tile_shape[1]
             tiles[t_row, :, :] = arr[row_start:row_end, col_start:col_end]
             t_row += 1
 
@@ -105,12 +108,12 @@ def tile(
 
 
 def pad(
-        arr:            np.ndarray,
-        tile_shape:     Tuple[int, int],
-        value:          float = 0.0,
-        extra_padding:  int   = 0,
-        even_pad:       bool  = False,
-        ) -> np.ndarray:
+    arr:            np.ndarray,
+    tile_shape:     Tuple[int, int],
+    value:          float = 0.0,
+    extra_padding:  int   = 0,
+    even_pad:       bool  = False,
+) -> np.ndarray:
 
     """
     Pad an array with a given value so that it can be tiled.
@@ -150,37 +153,10 @@ def pad(
     return arr_padded
 
 
-def merge_offset(arr1: np.ndarray, arr2: np.ndarray, x_offset: int, y_offset: int) -> np.ndarray:
-
-    """
-    Merge two arrays which were both tiled from the same array, where one was tiled with offset.
-
-    Parameters:
-    -----------
-    arr1 : np.ndarray
-        This is the array that was tiled without offset.
-    arr2 : np.ndarray
-        This is the array that was tiled with offset.
-    x_offset : int
-        This is the amount that the tiling was offset in the x (column) direction.
-    y_offset : int
-        This is the amount that the tiling was offset in the y (row) direction.
-
-    Returns:
-    --------
-    merged_arr : np.ndarray(shape=())
-        This is the merged array.
-    """
-
-    rows = arr2.shape[0]
-    cols = arr2.shape[1]
-    arr1_part = arr1[0:(rows - y_offset), 0:(cols - x_offset)]
-    arr2_part = arr2[y_offset:rows, x_offset:cols]
-    merged_arr = (arr1_part + arr2_part) / 2
-    return merged_arr
-
-
-def simulate_unet_cropping(arr: np.ndarray, crop_shape: tuple):
+def simulate_unet_cropping(
+    arr: np.ndarray,
+    crop_shape: tuple
+) -> np.ndarray:
 
     """
     Symmetrically crop the inputed array.
@@ -208,17 +184,15 @@ def simulate_unet_cropping(arr: np.ndarray, crop_shape: tuple):
 
 
 def tiles_to_image(
-            arr:            np.ndarray,
-            rows:           int,
-            cols:           int,
-            original_shape: Tuple[int, int],
-            cropped:        bool = False,
-            tiling_size:    int = 0
-            ) -> np.ndarray:
+    arr:            np.ndarray,
+    rows:           int,
+    cols:           int,
+    original_shape: Tuple[int, int]
+) -> np.ndarray:
 
     """
     Stich an array of 2-dimensional tiles into a single 2-dimensional array.
-
+s
     Parameters:
     -----------
     arr : np.ndarray(shape=(rows*cols, tile_size, tile_size))
@@ -241,13 +215,10 @@ def tiles_to_image(
         The stiched 2-dimensional array.
     """
 
-    print(original_shape)
-    print(cropped)
-    print(tiling_size)
-
-    # These should be equal
     row_tile_size = len(arr[0, :, 0])
     col_tile_size = len(arr[0, 0, :])
+
+    assert row_tile_size == col_tile_size
 
     tile_size = row_tile_size
 
@@ -258,204 +229,16 @@ def tiles_to_image(
             col = j * tile_size
             rebuilt_arr[row:row + tile_size, col:col + tile_size] = arr[(i * cols) + j]
 
-    if cropped:
-        crop_size = tile_size
-        tile_size = tiling_size
-        cl = (tile_size - crop_size) // 2
-    else:
-        crop_size = 0
-        cl = 0
-
     (y, x) = original_shape
 
-    x_padded = x + (tile_size - (x % tile_size)) + crop_size
-    y_padded = y + (tile_size - (y % tile_size)) + crop_size
+    x_padded = x + (tile_size - (x % tile_size))
+    y_padded = y + (tile_size - (y % tile_size))
 
     start_row = ceil((y_padded - y) / 2)
     end_row = (start_row + y)
     start_col = ceil((x_padded - x) / 2)
     end_col = (start_col + x)
 
-    rebuilt_arr = rebuilt_arr[start_row - cl:end_row - cl, start_col - cl:end_col - cl]
+    rebuilt_arr = rebuilt_arr[start_row:end_row, start_col:end_col]
 
     return rebuilt_arr
-
-# ------------------------------ #
-# Experimental Merging Functions #
-# ------------------------------ #
-
-
-# Merge Tiles
-def merge_tiles(tile_a: np.ndarray, tile_b: np.ndarray):
-
-    """
-    Experimental function that supports unidirectional_merge_tiles() -- Do not use right now.
-
-    Parameters:
-    -----------
-    tile_a : np.ndarray
-        tile from an array of tiles
-    tile_b : np.ndarray
-        tile from an array of tiles
-
-    Returns:
-    --------
-    tile_a : np.ndarray
-        tile_a merged with tile_b
-    """
-
-    delta = np.mean(tile_b - tile_a)
-    jumpval = 2 * np.pi * np.round(delta / (2 * np.pi))
-    tile_a += jumpval
-    return tile_a
-
-
-# Unidirectionally Merge Tiles
-def unidirectional_merge_tiles(tiles: np.ndarray):
-
-    """
-    Experimental tile merging algoritm -- Do not use right now.
-
-    Parameters:
-    -----------
-    tiles : np.ndarray
-        3-dimensional array of tiles that needs to be merged
-
-    Returns:
-    --------
-    tiles : np.ndarray
-        2-dimensional array stiched from the merging of the tiles
-    """
-
-    rows = tiles.shape[0]
-    cols = tiles.shape[1]
-    for iw in range(cols):
-        for j in range(rows):
-            try:
-                tiles[j, iw] = merge_tiles(tiles[j, iw], tiles[j - 1, iw])
-            except IndexError:
-                print("Nothing Above: {", iw, ", ", j, "}")
-            try:
-                tiles[j, iw] = merge_tiles(tiles[j, iw], tiles[j, iw - 1])
-            except IndexError:
-                print("Nothing to the Left: {", iw, ", ", j, "}")
-    return tiles
-
-
-def get_junctions(position: tuple):
-
-    """
-    Experimental function that supports tsrncp_merger() -- Do not use right now.
-
-    Parameters:
-    -----------
-    position : tuple
-        tuple representing the dimensions of an array of tiles
-
-    Returns:
-    --------
-    final : list[list[tuple]]
-        list of verticies representing the possible junctions of tiles
-    """
-
-    a = position[0]
-    b = position[1]
-    arr = [[(a, b), (a, b + 1)], [(a, b), (a + 1, b)], [(a, b), (a, b - 1)], [(a, b), (a - 1, b)]]
-    final = arr
-    i = 0
-    for i in range(len(arr)):
-        if arr[i][1][0] < 0 or arr[i][1][1] < 0:
-            final.remove(arr[i])
-    return final
-
-
-def get_junctions_(dims: tuple):
-
-    """
-    Experimental function that supports tsrncp_merger() -- Do not use right now.
-
-    Parameters:
-    -----------
-    dims : tuple
-        tuple representing the dimensions of an array of tiles
-
-    Returns:
-    --------
-    verts : list
-        list of verticies representing the possible junctions of tiles
-    """
-
-    h = dims[0]
-    w = dims[1]
-    verts = [ ]
-    for j in range(h):
-        for i in range(w):
-            if j + 1 < h:
-                verts.append([(j, i), (j + 1, i)])
-            if i + 1 < w:
-                verts.append([(j, i), (j, i + 1)])
-    return verts
-
-
-def tsrncp_merger(tiles: np.ndarray):
-
-    """
-    Experimental merging algorithm -- Do not use right now.
-
-    Parameters:
-    -----------
-    tiles : np.ndarray
-        3-dimensional array of tiles which need to be merged
-
-    Returns:
-    --------
-    tiles : np.ndarray
-        2-dimensional array of merged tiles
-    """
-
-    dims = tiles.shape
-    L = np.asarray(get_junctions_(dims))
-    reliability = []
-    for J in L:
-        r = 1 / np.var(tiles[tuple(J[1])] - tiles[tuple(J[0])])
-        reliability.append(r)
-    sorted_indecies = np.argsort(reliability)
-    rev_sorted_indecies = list(reversed(sorted_indecies))
-    L_sorted = L[rev_sorted_indecies]
-    groups = [ [ ] ]
-    for junction in L_sorted:
-
-        j0 = tuple(junction[0])
-        j1 = tuple(junction[1])
-
-        find_a = [x for x in groups if j0 in x]
-        find_b = [x for x in groups if j1 in x]
-
-        if len(find_a) == 0 and len(find_b) == 0:
-            tiles[j0] = merge_tiles(tiles[j0], tiles[j1])
-            groups.append([j0, j1])
-
-        elif len(find_a) == 0 and len(find_b) > 0:
-            tiles[j0] = merge_tiles(tiles[j0], tiles[j1])
-            groups[groups.index(find_b[0])].append(j0)
-
-        elif len(find_a) > 0 and len(find_b) == 0:
-            tiles[j0] = merge_tiles(tiles[j1], tiles[j0])
-            groups[groups.index(find_a[0])].append(j1)
-
-        else:
-            if groups[groups.index(find_a[0])] != groups[groups.index(find_b[0])]:
-                index_a = groups.index(find_a[0])
-                index_b = groups.index(find_b[0])
-                if len(groups[index_a]) > len(groups[index_b]):
-                    for tile in groups[index_b]:
-                        tiles[j0] = merge_tiles(tiles[j0], tiles[tile])
-                        groups[index_a].append(tile)
-                    groups.pop(index_b)
-                else:
-                    for tile in groups[index_a]:
-                        tiles[j1] = merge_tiles(tiles[j1], tiles[tile])
-                        groups[index_b].append(tile)
-                    groups.pop(index_a)
-
-    return tiles
