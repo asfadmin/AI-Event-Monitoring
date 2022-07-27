e272282b4841ed1582f454443d3a0731d187a5df
Andrew Player <andrewplayer3@gmail.com>
2022-07-27 13:02:13 -0800

    cleaning

--- src/architectures/unet.py
+++ src/architectures/unet.py
@@ -3,13 +3,11 @@
  File Name:    model.py
  Date Created: 01-25-2021
  Description:  Contains a network for sar classification
- Credits: https://arxiv.org/pdf/2010.13268.pdf
 """
 
 
 from tensorflow                  import Tensor
-from tensorflow.keras.layers     import Activation, AveragePooling2D, BatchNormalization, Bidirectional
-from tensorflow.keras.layers     import Conv2D, Conv2DTranspose, Input, concatenate, Cropping2D
+from tensorflow.keras.layers     import Conv2D, Conv2DTranspose, MaxPooling2D, Input, concatenate
 from tensorflow.keras.models     import Model
 from tensorflow.keras.optimizers import Adam 
 
@@ -20,28 +18,24 @@ def conv2d_block(
 ) -> Tensor:
 
     """
-    UNET-ish 2D-Convolution Block for encoding / generating feature maps.
+    UNET style 2D-Convolution Block for encoding / generating feature maps.
     """
 
     x = Conv2D(
         filters            = num_filters,
         kernel_size        = (3, 3)     ,
         kernel_initializer = 'he_normal',
-        padding            = 'same'
+        padding            = 'same',
+        activation         = 'relu'
     )(input_tensor)
 
     x = Conv2D(
         filters            = num_filters,
         kernel_size        = (3, 3)     ,
         kernel_initializer = 'he_normal',
-        padding            = 'same'
-    )(input_tensor)
-
-    x = BatchNormalization()(x)
-    
-    x = Activation('relu')  (x)
-    
-    x = AveragePooling2D()  (x)
+        padding            = 'same',
+        activation         = 'relu'
+    )(x)
 
     return x
 
@@ -55,23 +49,19 @@ def transpose_block(
     """
     Learned Upscaling for decoding
     """
-    
+
     x = Conv2DTranspose(
         filters      = num_filters,
-        kernel_size  = (3, 3),
+        kernel_size  = (2, 2),
         strides      = (2, 2),
         padding      = 'same'
     )(input_tensor)
     
-    # #TODO: Implement this
-    # x = Cropping2D(cropping=((1, 1), (1, 1)))(x)
-    
     x = concatenate([x, concat_tensor])
     
-    x = conv2d_block(x, num_filters)
-    x = conv2d_block(x, num_filters)
+    y = conv2d_block(x, num_filters)
 
-    return x
+    return y
 
 
 def create_unet(
@@ -83,32 +73,32 @@ def create_unet(
 ) -> Model:
 
     """
-    Creates a model combining a UNET style encoder-decoder with an LSTM.
-
-    References:
-    -----------
-    https://arxiv.org/pdf/2010.13268.pdf
+    Creates a U-Net style model
     """
 
     input = Input(shape = (tile_size, tile_size, 1))
 
-    print("*#*#*#*#*#*#*#*#*#*# SHAPE: ", input.shape)
 
     # --------------------------------- #
     # Feature Map Generation            #
     # --------------------------------- #
 
-    c1 = conv2d_block(input, num_filters * 1)
-    c2 = conv2d_block(c1   , num_filters * 2)
-    c3 = conv2d_block(c2   , num_filters * 4)
-    c4 = conv2d_block(c3   , num_filters * 8)
+    c1 = conv2d_block(input, num_filters *  1)
+    m1 = MaxPooling2D((2, 2), strides=2)  (c1)
+    c2 = conv2d_block(m1   , num_filters *  2)
+    m2 = MaxPooling2D((2, 2), strides=2)  (c2)
+    c3 = conv2d_block(m2   , num_filters *  4)
+    m3 = MaxPooling2D((2, 2), strides=2)  (c3)
+    c4 = conv2d_block(m3   , num_filters *  8)
+    m4 = MaxPooling2D((2, 2), strides=2)  (c4)
+    c5 = conv2d_block(m4   , num_filters * 16)
 
 
     # --------------------------------- #
     # Learned Upscaling                 #
     # --------------------------------- #
 
-    u8  = transpose_block(c4 , c4, num_filters * 8)
+    u8  = transpose_block(c5 , c4, num_filters * 8)
     u9  = transpose_block(u8 , c3, num_filters * 4)
     u10 = transpose_block(u9 , c2, num_filters * 2)
     u11 = transpose_block(u10, c1, num_filters * 1)
@@ -119,11 +109,11 @@ def create_unet(
     # --------------------------------- #
 
     output = Conv2D(
-        name       = 'last_layer',
-        kernel     = (1, 1),
-        filters    =  1    ,
-        activation = 'linear',
-        padding    = 'same'
+        name        = 'last_layer',
+        kernel_size = (1, 1),
+        filters     =  1    ,
+        activation  = 'linear',
+        padding     = 'same'
     )(u11)
 
 
