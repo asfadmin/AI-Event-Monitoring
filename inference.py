3fd60d5c8dcb70cd106c6ee21dfe137b430ca699
Andrew Player <andrewplayer3@gmail.com>
2023-03-16 15:01:34 -0500

    api script

---
+++ inference.py
@@ -0,0 +1,24 @@
+import requests
+import click
+@click.group()
+def cli():
+    pass
+
+
+@cli.command ("predict-event")
+@click.argument("usgs-event-id", type=str)
+@click.argument("product-name", type=str)
+def model_inference(usgs_event_id, product_name):
+ url = "https://aevrv4z4vf.execute-api.us-west-2.amazonaws.com/test-2/predict-event"
+
+ r = requests.post(
+ url,
+ json={
+  "usgs_event_id": usgs_event_id,
+  "product_name": product_name
+ })
+ print(r.json())
+
+
+if __name__ == "__main__":
+ cli()
