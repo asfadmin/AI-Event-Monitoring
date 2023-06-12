fc6d4b668a5d8471d4b4dfa1222ed7b8348e5e5f
Andy Kozy <canooka143@hotmail.com>
2023-06-12 14:57:24 -0800

    adding perlin noise generation

--- src/synthetic_interferogram.py
+++ src/synthetic_interferogram.py
@@ -9,7 +9,6 @@ import random
 from typing import Tuple
 
 import numpy as np
-from perlin_numpy import generate_perlin_noise_2d
 from astropy.modeling.models import Gaussian2D
 from src.processing import simulate_unet_cropping
 
@@ -76,14 +75,42 @@ def generate_perlin(size: int) -> np.ndarray:
 
     """
 
+    resolution = (res, res)
+    shape = (size, size)
+    delta = (resolution[0] / shape[0], resolution[1]/shape[1])
+    delta2 = (shape[0] // resolution[0], shape[1] // resolution[1])
+
+    grid = np.mgrid[0:resolution[0]:delta[0], 0:resolution[1]:delta[1]].transpose(1, 2, 0) % 1
+    interpolated_grid = np.power(grid, 3) * (grid * (grid * 6 - 15) + 10)
+
+    angles = 2 * np.pi * np.random.rand(resolution[0] + 1, resolution[1] + 1)
+    gradients = np.dstack((np.cos(angles), np.sin(angles)))
+    gradients = gradients.repeat(delta2[0], 0).repeat(delta2[1], 1)
+
+    gradients00 = gradients[:-delta2[0],:-delta2[1]]
+    ramp00 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]  )) * gradients00, 2)
+
+    gradients10 = gradients[delta2[0]:,:-delta2[1]]
+    ramp10 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]  )) * gradients10, 2)
+
+    gradients01 = gradients[:-delta2[0],delta2[1]:]
+    ramp01 = np.sum(np.dstack((grid[:,:,0]  , grid[:,:,1]-1)) * gradients01, 2)
+    
+    gradients11 = gradients[delta2[0]:,delta2[1]:]
+    ramp11 = np.sum(np.dstack((grid[:,:,0]-1, grid[:,:,1]-1)) * gradients11, 2)
+
+    ramps0 = (ramp00*(1-interpolated_grid[:,:,0]) + ramp10*interpolated_grid[:,:,0])*(1-interpolated_grid[:,:,1])
+    ramps1 = (ramp01*(1-interpolated_grid[:,:,0]) + ramp11*interpolated_grid[:,:,0])*(interpolated_grid[:,:,1])
+
+    ramps = ramps0+ramps1
+    p_noise = np.sqrt(2)*ramps
+
     perlin_array = np.zeros((size, size))
     for j in range(0, 3):
         for i in range(4 * (2**j), size):
             if size % i == 0:
                 res = i
-                perlin_array += generate_perlin_noise_2d(
-                    (size, size), (res, res), (True, True)
-                ) * (2 ** (3 - j))
+                perlin_array += p_noise * (2 ** (3 - j))
                 break
     min = np.amin(perlin_array)
     perlin_array -= min
