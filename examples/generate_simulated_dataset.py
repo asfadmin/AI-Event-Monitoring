8a83e04542b612ae69a35794212ce922898ef2db
Lily Larsen <8o7wermobile@gmail.com>
2023-06-28 12:45:06 -0800

    fix examples

--- examples/generate_simulated_dataset.py
+++ examples/generate_simulated_dataset.py
@@ -1,17 +1,15 @@
-import matplotlib.pyplot as plt
-
 from insar_eventnet.config import SYNTHETIC_DIR
 from insar_eventnet.io import make_simulated_dataset, split_dataset, create_directories
 
 name = "simulated_dataset"
 
 amount = 2000
-seed = 0 # if set to 0, the seed is randomized each run
+seed = 0  # if set to 0, the seed is randomized each run
 tile_size = 512
 crop_size = 512
-split = 0.2 # Training/Testing split
+split = 0.2  # Training/Testing split
 
-create_directories() # Initialize directory structure for training data
+create_directories()  # Initialize directory structure for training data
 
 name, count, dir_name, distribution, dataset_info = make_simulated_dataset(
     name, SYNTHETIC_DIR, amount, seed, tile_size, crop_size
--- examples/infer_mask.py
+++ examples/infer_mask.py
@@ -1,32 +1,40 @@
-from tensorflow.keras.models import load_model
-from insar_eventnet.inference import mask, plot_results
-from insar_eventnet.io import initialize
+import matplotlib.pyplot as plt
+
+from insar_eventnet.inference import mask_image_path
+from insar_eventnet.io import initialize, get_image_array
 
 tile_size = 512
 crop_size = 512
 
 mask_model_path = "data/output/models/mask_model"
 pres_model_path = "data/output/models/pres_model"
-image_path = input("Image Path: ") # Prompt user for input interferogram
+image_path = input("Image Path: ")  # Prompt user for input interferogram
 image_name = image_path.split("/")[-1].split(".")[0]
 output_path = f"masks_inferred/{image_name}_mask.tif"
+image, gdal_dataset = get_image_array(image_path)
 
 # The initialize function downloads the pretrained models
 initialize()
-mask_model = load_model(mask_model_path)
-pres_model = load_model(pres_model_path)
 
-mask, presence = mask(
-    model_path=mask_model_path,
+mask, presence = mask_image_path(
+    mask_model_path=mask_model_path,
     pres_model_path=pres_model_path,
-    product_path=image_path,
+    image_path=image_path,
     tile_size=tile_size,
     crop_size=crop_size,
 )
 
-if np.mask(presence) > 0.7:
+if presence > 0.7:
     print("Positive")
 else:
     print("Negative")
 
-plot_results(wrapped, mask, presence)
+_, [axs_wrapped, axs_mask] = plt.subplots(1, 2, sharex=True, sharey=True)
+
+axs_wrapped.set_title("Wrapped")
+axs_mask.set_title("Segmentation Mask")
+
+axs_wrapped.imshow(image, origin="lower", cmap="jet")
+axs_mask.imshow(mask, origin="lower", cmap="jet")
+
+plt.show()
